3842ba487bd850228c969190a87a4c42c9520fa4
Nathan Hoad <nathan@getoffmalawn.com>
2012-05-27 20:44:43 +1000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+settings = {
+    'name': 'outbox',
+    'version': '0.1',
+    'description': 'SMTP client for Humans.',
+    'long_description': open('README.rst').read(),
+    'author': 'Nathan Hoad',
+    'author_email': 'nathan@getoffmalawn.com',
+    'url': 'https://bitbucket.org/getoffmalawn/outbox.py',
+    'py_modules': ['outbox',],
+    'license': 'BSD',
+    'classifiers': (
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        # 'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+    )
+}
+
+setup(**settings)
