aa5a18f7fc0372c441184eabc965526fd3330c71
Nathan Hoad <nathan@getoffmalawn.com>
2012-07-31 16:33:07 +1000

    Version bump in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.argv[-1] == 'publish':
 
 settings = {
     'name': 'outbox',
-    'version': '0.1.2',
+    'version': '0.1.3',
     'description': 'SMTP client for Humans.',
     'long_description': open('README.rst').read(),
     'author': 'Nathan Hoad',
